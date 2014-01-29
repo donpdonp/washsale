96adb40af4b30d54f451da512f95055813898d2f
Don Park <donp@donp.org>
2014-01-29 09:56:42 -0800

    mucho less debug

--- lib/wash_sale.rb
+++ lib/wash_sale.rb
@@ -26,7 +26,6 @@ class WashSale
     raise "Insufficient dollars of #{"%0.2f"%fiat.total.to_f} to buy #{"%0.2f"%value.to_f}" if fiat.total < value
     coins << Statement.new({time:record.time, amount:record.amount, price: record.price,
                             txid: record.txid, fee: record.fee})
-puts "Buy is removing value #{"%0.3f"%value} from #{record.txid}"
     @fiat.remove(value)
   end
 
--- load.rb
+++ load.rb
@@ -116,15 +116,17 @@ records.each do |record|
     usd_adjust = deposit_total - withdraw_total
     if record.action == "earned"
       calc_error = (record.fee_balance - (fiat.total + usd_adjust)).abs
-      puts "!! sell calculation error csv fee USD balance #{"%0.2f"%record.fee_balance} - #{"%0.2f"%fiat.total} + #{"%0.2f"%deposit_total} - #{"%0.2f"%withdraw_total} = #{"%0.8f"%calc_error}"
+      if calc_error > 0.000001
+        puts "!! sell calculation error csv fee USD balance #{"%0.2f"%record.fee_balance} - #{"%0.2f"%fiat.total} + #{"%0.2f"%deposit_total} - #{"%0.2f"%withdraw_total} = #{"%0.8f"%calc_error}"
+      end
       washer.tax_check(sale_results, record.time)
     end
     if record.action == "spent"
       calc_error = (record.fee_balance - (coins.total + btc_adjust)).abs
-      puts "!! buy calculation error csv fee BTC balance #{"%0.4f"%record.fee_balance} - #{"%0.4f"%coins.total} + #{"%0.4f"%deposit_btc_total} - #{"%0.4f"%withdraw_btc_total} = #{"%0.8f"%calc_error}"
+      if calc_error > 0.000001
+        puts "!! buy calculation error csv fee BTC balance #{"%0.4f"%record.fee_balance} - #{"%0.4f"%coins.total} + #{"%0.4f"%deposit_btc_total} - #{"%0.4f"%withdraw_btc_total} = #{"%0.8f"%calc_error}"
+      end
     end
-    puts "** coins total: #{record.time.strftime("%Y-%m-%d %H:%M:%S")} #{"%0.4f"%(coins.total+btc_adjust)}"
-    puts "** fiat total: #{record.time.strftime("%Y-%m-%d %H:%M:%S")} #{"%0.4f"%(fiat.total+usd_adjust)}"
   end
 end
 
